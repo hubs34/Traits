f7334f87a2cbdee0b7158e473cd86e4281dc58b8
hubs34 <71785637+hubs34@users.noreply.github.com>
2020-09-26 21:18:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    previous_turn += -1
+    basic.showNumber(previous_turn)
+})
+input.onButtonPressed(Button.AB, function () {
+    previous_turn = 0
+    if (true) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    previous_turn += 1
+    basic.showNumber(previous_turn)
+})
+let previous_turn = 0
+basic.showString("traits ")
+previous_turn = 0
